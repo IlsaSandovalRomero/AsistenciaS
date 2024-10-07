ce537af1ba0f63f2e9b066fd6656c009de9c1389
Francisco Pilarte <fpilarteespinosa@gmail.com>
2024-10-07 11:03:46 -0600

    Ingresar los datos del estudiante: nombre, asistencia total de la clase y los totales de sesiones asistidas (validando los datos)

--- Program.cs
+++ Program.cs
@@ -23,6 +23,52 @@ namespace Program
             } while (cantEstudiantes <= 0);
 
             Universidad.Estudiante[] estudiantes = new Universidad.Estudiante[cantEstudiantes];
+
+            for (int i = 0; i < cantEstudiantes; i++)
+            {
+                Console.WriteLine($"Por favor, ingrese los datos del estudiante {i + 1}:");
+
+                // Ingresar nombre del estudiante
+
+                Console.WriteLine($"Nombre del estudiante {i + 1}: ");
+                string nombre = Console.ReadLine();
+
+                // Ingresar y validar el total de sesiones
+
+                int totalDeSesiones;
+                do
+                {
+                    Console.Write("Ingrese el total de sesiones del curso: ");
+                    totalDeSesiones = Convert.ToInt32(Console.ReadLine());
+
+                    if (totalDeSesiones <= 0)
+                    {
+                        Console.WriteLine("El total de sesiones debe ser un numero mayor que 0. Intentelo de nuevo, por favor.");
+                    }
+                    else if (totalDeSesiones > 200)
+                    {
+                        Console.WriteLine("El total de sesiones debe ser un numero menor que 200. Intentelo de nuevo.");
+
+                    }
+
+                } while (totalDeSesiones <= 0);
+
+
+                // Ingresar y validar las sesiones asistidas
+
+                int totalSesionesAsistidas;
+                do
+                {
+                    Console.Write("Sesiones asistidas: ");
+                    totalSesionesAsistidas = Convert.ToInt32(Console.ReadLine());
+
+                    if (totalSesionesAsistidas < 0 || totalSesionesAsistidas > totalDeSesiones)
+                    {
+                        Console.WriteLine($"El número de sesiones asistidas debe estar entre 0 y {totalDeSesiones}. Inténtelo de nuevo.");
+                    }
+
+                } while (totalSesionesAsistidas < 0 || totalSesionesAsistidas > totalDeSesiones);
+            }
         }
     }
 }
