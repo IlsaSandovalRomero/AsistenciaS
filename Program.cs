8cc0ca64440476966028ece8f05c6bfa9b49b8b7
Francisco Pilarte <fpilarteespinosa@gmail.com>
2024-10-07 11:00:20 -0600

    Crear un dowhile para ingresar la cantidad de estudiantes y validar el dato, crear el arreglo Universidad.Estudiante[] estudiante ingresar los datos

--- Program.cs
+++ Program.cs
@@ -8,7 +8,21 @@ namespace Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("¡Bienvenido al sistema de registro y control de asistencia de estudiantes de la Universidad Nacional de Ingenieria!");
 
+            int cantEstudiantes;
+            do
+            {
+                Console.WriteLine("Ingrese la cantidad de estudiantes: ");
+                cantEstudiantes = Convert.ToInt32(Console.ReadLine());
+
+                if (cantEstudiantes <= 0)
+                {
+                    Console.WriteLine("Por favor, ingrese un numero valido de estudiantes: ");
+                }
+            } while (cantEstudiantes <= 0);
+
+            Universidad.Estudiante[] estudiantes = new Universidad.Estudiante[cantEstudiantes];
         }
     }
 }
