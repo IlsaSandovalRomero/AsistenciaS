b7fc18811f724d9073d2f21496009510246fc220
Francisco Pilarte <fpilarteespinosa@gmail.com>
2024-10-07 10:48:41 -0600

    Crear la clase Universidad,  crear la clase anidada estudiante

---
+++ Universidad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Clase Universidad
+public static class Universidad
+{
+    // Clase anidada Estudiante
+    public class Estudiante
+    {
+
+    }
+
+
+}
