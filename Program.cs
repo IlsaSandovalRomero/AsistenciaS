4d84f6611bf0d4f86d00065f0414d5f735a5c7bc
Francisco Pilarte <fpilarteespinosa@gmail.com>
2024-10-07 10:46:17 -0600

    Escribir el using System, escribir en namespace, crear la clase Program, crear la clase statica Main

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
 ﻿//C:\Users\MSI\source\repos
+using System;
+using System.Collections.Generic;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
