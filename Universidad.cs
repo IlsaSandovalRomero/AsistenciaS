a5cb11338a73bfc0a82dc4f65ce9d9c8da1dd838
Francisco Pilarte <fpilarteespinosa@gmail.com>
2024-10-07 10:50:52 -0600

    Crear método estático para calcular el porcentaje de asistencia

--- Universidad.cs
+++ Universidad.cs
@@ -7,7 +7,19 @@ public static class Universidad
     // Clase anidada Estudiante
     public class Estudiante
     {
+        public string Nombre { get; set; }
+        public int TotalDeSesiones { get; set; }
+        public int totalSesionesAsistidas { get; set; }
 
+        // Clase interna Asistencia para calcular el porcentaje y si aprueba
+        public class Asistencia
+        {
+
+
+
+
+
+        }
     }
 
 
