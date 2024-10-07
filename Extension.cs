8abbfb74a84825bbc8e071dbb8cfa48c1e02160e
Francisco Pilarte <fpilarteespinosa@gmail.com>
2024-10-07 11:10:21 -0600

    final

--- Extension.cs
+++ Extension.cs
@@ -3,24 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static Universidad;
 
 namespace Asistencia
 {
+
+    // Clase para el método de extensión....
     public static class Extension
     {
+        // Método de extensión para mostrar la información del estudiante
         public static void MostrarInfo(this Universidad.Estudiante estudiante)
-double porcentajeDeAsistencia = Universidad.Estudiante.Asistencia.Porcentaje(estudiante.totalSesionesAsistidas, estudiante.TotalDeSesiones);
-        bool minimoAsis = Universidad.Estudiante.Asistencia.MinimoDeAsistencia(porcentajeDeAsistencia);
+        {
+            double porcentajeDeAsistencia = Universidad.Estudiante.Asistencia.Porcentaje(estudiante.totalSesionesAsistidas, estudiante.TotalDeSesiones);
+            bool minimoAsis = Universidad.Estudiante.Asistencia.MinimoDeAsistencia(porcentajeDeAsistencia);
 
-        Console.WriteLine();
-Console.WriteLine($"Estudiante: {estudiante.Nombre}");
-Console.WriteLine($"Asistencia: {porcentajeDeAsistencia:F2}%");
-Console.WriteLine($"Asistio a {estudiante.totalSesionesAsistidas} de {estudiante.TotalDeSesiones} sesiones programadas);
-Console.WriteLine($"Estado: { (minimoAsis ? Aprobado por asistencia" :  "Reprobado por inasistencia")}");
-Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"Estudiante: {estudiante.Nombre}");
+            Console.WriteLine($"Asistencia: {porcentajeDeAsistencia:F2}%");
+            Console.WriteLine($"Asistio a {estudiante.totalSesionesAsistidas} de {estudiante.TotalDeSesiones} sesiones programadas.");
+            Console.WriteLine($"Estado: {(minimoAsis ? "Aprobado por asistencia" : "Reprobado por inasistencia")}");
+            Console.WriteLine();
+        }
 
 
-
-      }
+    }
 }
